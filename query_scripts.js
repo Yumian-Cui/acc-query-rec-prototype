ff5d9c0b32ad996c83555b993d714b89209afa0a
Yumian Cui <59884821+Yumian-Cui@users.noreply.github.com>
2022-08-23 12:25:41 +0000

    Changes I've made: Reformatted some styles? Well, not great at CSS, but managed to change some attributes, including the CSS grid of the data table; Added Voice Typing and Answering functionality; Turned most elements to be accessible (able to use tab key to navigate around); The recommendation section --- just the skeleton; Add one more hardcoding answer for the big trends at the beginning; A few slight and visible changes...

---
+++ query_scripts.js
@@ -0,0 +1,139 @@
+/* For parsing file input */
+const fileInput = document.getElementById('dataset'); 
+const warning = document.getElementById('warning_output'); 
+var CSVARRAY;
+
+fileInput.addEventListener('change', function() {
+  var reader = new FileReader();
+  var f = fileInput.files[0];
+
+  warning.textContent = '';
+
+  if (f.type != 'text/csv') {
+    warning.textContent += 'Please select a csv file';
+
+  } else {
+    reader.onload = function(e) {
+      console.log(e.target.result);
+      CSVARRAY = parseResult(e.target.result); //this is where the csv array will be
+    };
+    reader.readAsText(f);
+  }
+});
+
+function parseResult(result) {
+    var resultArray = [];
+    result.split("\n").forEach(function(row) {
+        var rowArray = [];
+        row.split(",").forEach(function(cell) {
+            rowArray.push(cell);
+        });
+        resultArray.push(rowArray);
+    });
+    return resultArray;
+}
+
+function createTable() {
+    var array = CSVARRAY;
+    var content = "";
+    array.forEach(function(row) {
+        content += "<tr>";
+        row.forEach(function(cell) {
+            content += "<td>" + cell + "</td>" ;
+        });
+        content += "</tr>";
+    });
+    document.getElementById("datatable").innerHTML = content;
+}
+
+/* For processing messages */
+const inputField = document.getElementById("chat_text_input"); 
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("#chat_text_input").addEventListener("keydown", function(e) {
+    if (e.code === "Enter") {
+      let input = inputField.value; // store the value
+      inputField.value = ""; // clear the text input
+      outputEntry(input); // print out the output 
+    }
+  });
+});
+
+function botResponse(input) {
+  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+
+  // if (!input) { //before users pose any questions
+  //   return "Below is a summary of the big trends of this dataset:\n1) ...\n2) ....";
+  // }
+  
+  let result = "I'm sorry, I do not understand your question.\nPlease input another question.";
+
+  var keywords = ["summary", "trend", "statistics", "pattern"];
+  if (keywords.indexOf(text) != -1) {
+    result = "Below is a summary of the big trends of this dataset:\n1) ...\n2) ....";
+  }
+
+  if (text.includes("min") || text.includes("minimum") 
+    || text.includes("least") || text.includes("smallest")) {
+    result = "The smallest value is 9.";
+  } 
+  if (text.includes("max") || text.includes("maximum") 
+    || text.includes("biggest") || text.includes("greatest") || text.includes("largest")) {
+    result = "The largest value is 9000.";
+  } 
+
+  return result;
+}
+
+function botSpeech(input) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = input;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  window.speechSynthesis.speak(speech);
+}
+
+function addUserInput(input) {
+  let userDiv = document.createElement("div");
+  userDiv.className = "user_message";
+  userDiv.innerHTML = `${input}`;
+  return userDiv;
+}
+
+function addBotInput(input) {
+  let botDiv = document.createElement("div");
+  let botText = document.createElement("span");
+  botDiv.className = "bot_message";
+  botText.innerText = botResponse(input);
+  botSpeech(botText.innerText);
+  botDiv.appendChild(botText);
+  return botDiv;
+}
+
+function outputEntry(input) {
+  const messagesContainer = document.getElementById("chat_body");
+
+  messagesContainer.appendChild(addUserInput(input));
+  messagesContainer.appendChild(addBotInput(input));
+
+  messagesContainer.scrollTop =
+    messagesContainer.scrollHeight - messagesContainer.clientHeight;
+}
+
+function voiceToText() {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recorder = new SpeechRecognition();
+
+  recorder.onspeechend = () => {
+    recorder.stop();
+  }
+  
+  recorder.onresult = (event) => {
+    const resultIndex = event.resultIndex;
+    const transcript = event.results[resultIndex][0].transcript;
+    inputField.value = transcript;
+  }
+
+  recorder.start();
+}
